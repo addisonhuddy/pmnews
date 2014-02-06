d3074c7401c267c13dae8d848ed05e1c8e114ccf
Addison Huddy <addisonhuddy@gmail.com>
2014-02-05 18:27:55 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+#Welcome to Product Manager news
+###It's like Hacker News put specfically for PM's!
+
+What you will find here:
+
+* Relevant links to PM's
+* Job posting
+* Questions for discussion
+
+You must be logged in to comment, vote, or post links
+
+Please be respectful at all times.
+
+Made with all the love in the work in San Francision by @addisonhuddy
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pmnews::Application.config.secret_key_base = '52bc38fb903766fb35519be528637c835f9052b48c802bcec8c59bdc9f8bf040430874162c3bcbf8c5396ccb000da56f61c6a84bdc21e32c42b084c35a0556df'
